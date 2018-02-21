2e2ebda68c869a25de0c95dc9a029ca6d58d74e3
adrianoff <adrianov87@gmail.com>
2018-02-21 15:46:00 +0300

    perceptron1

---
+++ stepik/practice/one_layer/stepic_neural_networks_public-master/HW_1/hw_1.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as p3
+import numpy as np
+import random
+import time
+
+from functools import partial
+from ipywidgets import interact, RadioButtons, IntSlider, FloatSlider, Dropdown, BoundedFloatText
+from numpy.linalg import norm
+random.seed(42) # начальное состояние генератора случайных чисел, чтобы можно было воспроизводить результаты.
+
+from .Perceptron import Perceptron
+
+
+
+
+
+def create_perceptron(m):
+    """Создаём перцептрон со случайными весами и m входами"""
+    w = np.random.random((m, 1))
+    return Perceptron(w, 1)
