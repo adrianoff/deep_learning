8f6571ec62d254d3d7f3590e150aea21846c42e8
adrianoff <adrianov87@gmail.com>
2018-02-21 21:12:53 +0300

    neuron

---
+++ neuron/Neuron/Neuron.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+def sigmoid(x):
+    """сигмоидальная функция, работает и с числами, и с векторами (поэлементно)"""
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_prime(x):
+    """производная сигмоидальной функции, работает и с числами, и с векторами (поэлементно)"""
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+class Neuron:
+    def __init__(self, weights, activation_function=sigmoid, activation_function_derivative=sigmoid_prime):
+        """
+        weights - вертикальный вектор весов нейрона формы (m, 1), weights[0][0] - смещение
+        activation_function - активационная функция нейрона, сигмоидальная функция по умолчанию
+        activation_function_derivative - производная активационной функции нейрона
+        """
+
+        assert weights.shape[1] == 1, "Incorrect weight shape"
+
+        self.w = weights
+        self.activation_function = activation_function
+        self.activation_function_derivative = activation_function_derivative
+
+    def forward_pass(self, single_input):
+        """
+        активационная функция логистического нейрона
+        single_input - вектор входов формы (m, 1),
+        первый элемент вектора single_input - единица (если вы хотите учитывать смещение)
+        """
+
+        result = 0
+        for i in range(self.w.size):
+            result += float(self.w[i] * single_input[i])
+
+        return self.activation_function(result)
+
+    def summatory(self, input_matrix):
+        """
+        Вычисляет результат сумматорной функции для каждого примера из input_matrix.
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных.
+        Возвращает вектор значений сумматорной функции размера (n, 1).
+        """
+        return input_matrix.dot(self.w)
+
+    def activation(self, summatory_activation):
+        """
+        Вычисляет для каждого примера результат активационной функции,
+        получив на вход вектор значений сумматорной функций
+        summatory_activation - вектор размера (n, 1),
+        где summatory_activation[i] - значение суммматорной функции для i-го примера.
+        Возвращает вектор размера (n, 1), содержащий в i-й строке
+        значение активационной функции для i-го примера.
+        """
+        return self.activation_function(summatory_activation)
+
+    def vectorized_forward_pass(self, input_matrix):
+        """
+        Векторизованная активационная функция логистического нейрона.
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных.
+        Возвращает вертикальный вектор размера (n, 1) с выходными активациями нейрона
+        (элементы вектора - float)
+        """
+        return self.activation(self.summatory(input_matrix))
---
+++ neuron/neuron_learning.py
@@ -0,0 +1,3 @@
+from neuron.Neuron.Neuron import Neuron
+
+
