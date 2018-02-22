968e4bdfd3e1c5fdb50796bc4afaee0db4205510
adrianoff <adrianov87@gmail.com>
2018-02-22 20:46:42 +0300

    Neuron

--- neuron/Neuron/Neuron.py
+++ neuron/Neuron/Neuron.py
@@ -67,3 +67,85 @@ class Neuron:
         (элементы вектора - float)
         """
         return self.activation(self.summatory(input_matrix))
+
+    def update_mini_batch(self, X, y, learning_rate, eps):
+        before = J_quadratic(self, X, y)
+        G = compute_grad_analytically(self, X, y)
+        self.w = self.w - learning_rate * G
+        after = J_quadratic(self, X, y)
+        # если значение целевой функции до и после меньше eps, то алгоритм сошелся
+        return 1 if abs(before - after) < eps else 0
+
+    def SGD(self, X, y, batch_size, learning_rate=0.1, eps=1e-6, max_steps=200):
+        pass
+
+
+def J_quadratic(neuron, X, y):
+    """
+    Оценивает значение квадратичной целевой функции.
+    Всё как в лекции, никаких хитростей.
+
+    neuron - нейрон, у которого есть метод vectorized_forward_pass, предсказывающий значения на выборке X
+    X - матрица входных активаций (n, m)
+    y - вектор правильных ответов (n, 1)
+
+    Возвращает значение J (число)
+    """
+
+    assert y.shape[1] == 1, 'Incorrect y shape'
+
+    return 0.5 * np.mean((neuron.vectorized_forward_pass(X) - y) ** 2)
+
+
+def J_quadratic_derivative(y, y_hat):
+    """
+    Вычисляет вектор частных производных целевой функции по каждому из предсказаний.
+    y_hat - вертикальный вектор предсказаний,
+    y - вертикальный вектор правильных ответов,
+
+    В данном случае функция смехотворно простая, но если мы захотим поэкспериментировать
+    с целевыми функциями - полезно вынести эти вычисления в отдельный этап.
+
+    Возвращает вектор значений производной целевой функции для каждого примера отдельно.
+    """
+
+    assert y_hat.shape == y.shape and y_hat.shape[1] == 1, 'Incorrect shapes'
+
+    return (y_hat - y) / len(y)
+
+
+def compute_grad_analytically(neuron, X, y, J_prime=J_quadratic_derivative):
+    """
+    Аналитическая производная целевой функции
+    neuron - объект класса Neuron
+    X - вертикальная матрица входов формы (n, m), на которой считается сумма квадратов отклонений
+    y - правильные ответы для примеров из матрицы X
+    J_prime - функция, считающая производные целевой функции по ответам
+
+    Возвращает вектор размера (m, 1)
+    """
+
+    # Вычисляем активации
+    # z - вектор результатов сумматорной функции нейрона на разных примерах
+
+    z = neuron.summatory(X)
+    y_hat = neuron.activation(z)
+
+    # Вычисляем нужные нам частные производные
+    dy_dyhat = J_prime(y, y_hat)
+    dyhat_dz = neuron.activation_function_derivative(z)
+
+    # осознайте эту строчку:
+    dz_dw = X
+
+    # а главное, эту:
+    grad = ((dy_dyhat * dyhat_dz).T).dot(dz_dw)
+
+    # можно было написать в два этапа. Осознайте, почему получается одно и то же
+    # grad_matrix = dy_dyhat * dyhat_dz * dz_dw
+    # grad = np.sum(, axis=0)
+
+    # Сделаем из горизонтального вектора вертикальный
+    grad = grad.T
+
+    return grad
